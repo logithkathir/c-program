4b1b23e3f6a00ab97c9a8de70aa4fff63510b33b
logithkathir <kanniahjammuna@gmail.com>
2017-06-19 16:43:32 +0530

    evenorodd.c
    program for even or odd

---
+++ evenorodd.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#incude<conio.h>
+int main()
+{
+int n;
+{
+printf("enter the value of n");
+scanf("%d",&n);
+if(n%2==0)
+printf("The given number is even");
+else 
+printf("The given number is odd");
+}
+getch();
+}
