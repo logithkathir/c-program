dd9292f83f3cd59582402d70a4453e210ebe88d2
logithkathir <kanniahjammuna@gmail.com>
2017-06-20 15:04:21 +0530

    sumofNnumbers.C
    program to find sum of N numbers

---
+++ sumofNnumbers.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+int k,num,sum=o,n;
+for(k=1;k<=n;k++)
+printf("enter the number \n");
+scanf("%d",&num);
+{
+sum=sum+num;
+}
+printf("the sum of N numbers=%d,sum\n");
+return 0;
+}
