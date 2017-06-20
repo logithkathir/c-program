3d70e01270034265790bf8005e16f275e32929ee
logithkathir <kanniahjammuna@gmail.com>
2017-06-20 14:55:16 +0530

    sumofnaturalnumbers.C
    program to find the sum of natural numbers

---
+++ sumofnaturalnumbers.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+int  k,num,sum;
+printf("Enter the number \n");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("the sum of the natural numbers is:=%d,num,sum");
+getch();
+}
