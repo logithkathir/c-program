d71cf854ed60abcc965aea4f1d5ad14d912535fb
sasikala <kanniahjammuna@gmail.com>
2017-06-22 16:05:17 +0530

    powerofnumber.C
    program to find the power of number

---
+++ powerofnumber.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int(int n1,int n2)
+{
+int base,pow,result;
+printf("Enter the base value");
+scanf("%d",&base);
+printf("Enter the value of power");
+scanf(:%d",&pow);
+result=pow*(base,pow);
+printf("%d%d=%d",the result is:\n");
+}
+int pow(int base,int pow)
+{
+result=base*pow(base,pow-1);
+{
+if(result!=0)
+return 0;
+else
+return 1;
+}
