e7206c48271d791f8bf5ed0d7da4b228cfce179b
logithkathir <kanniahjammuna@gmail.com>
2017-06-22 09:43:57 +0530

    string to integer conversion
    c programme

---
+++ stringtoint.C
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int tostring[]
+int nain(){
+int n;
+char  a[100];                              
+printf("enter a string");
+scanf("%d",&n);
+n=tostring(a);
+printf("\nstring %sinteger%d");
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
