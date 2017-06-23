ab0d180fbece6288f435063306fe5e4e56db8f86
sasikala <kanniahjammuna@gmail.com>
2017-06-23 15:10:52 +0530

    evennumbersbetweentwointervals
    write the  c program to find the even numbers between the two numbers

---
+++ evennumbersbetweentwointervals.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int k,n;
+printf("Enter the interval to find the even numbers between them \n");
+scanf("%d",k);
+printf("print all the even numbers between them \n");
+for(k=1;k<n;k++)
+{
+if(k%2=0)
+{
+print("%d",k);
+}
+}
+return 0;
+}
