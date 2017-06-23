2a0865780303bbc4668e1a429715708850fd55a7
sasikala <kanniahjammuna@gmail.com>
2017-06-23 15:00:10 +0530

    oddnumbersbetweentwointervals.C
    write the program to find the odd number between two intervals.

---
+++ oddnumberbetweentwointervals.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int k,n;
+printf("print odd numbers between given interval: \n");
+scanf("%d",&n);
+print("print the odd numbers from 1 to the number given"0;
+for(k=1;k<n;k++)
+{
+if(k%2!=0)
+{
+printf("%d",k);
+}
+}
+return 0;
+}
