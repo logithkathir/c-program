87f6277492d8e8db61cbc42f924ef0ba7583d4c7
sasikala <kanniahjammuna@gmail.com>
2017-06-23 15:47:49 +0530

    checkamstrongornot.C
    write a program to check whether it is an amstrong number or not

---
+++ checkamstrongornot.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int r,num,temp,sum=0;
+printf("Enter the number \n");
+scanf("%d",&num);
+temp=num;
+while(temp!=0)
+{
+r==num%10;
+num=num/10;
+sum=sum+(r*r*r);
+{
+if(sum==temp)
+printf("Enter the given number is a Amstrong number \n");
+else
+printf("Enter the given number is not an Amstrong number \n");
+}
+}
+}
