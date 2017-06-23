65e62a8d5de7903ea1a5009cb1890e2d5d1dc671
sasikala <kanniahjammuna@gmail.com>
2017-06-23 15:31:52 +0530

    primenumbersbetweentwointervals.C
    write a program to find the prime numbers between the two intervals

---
+++ primenumbersbetweentwointervals.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int k,n,startinglimit,endinglimit;
+printf("Enter the startinglimit \n");
+scanf("%d",startinglimit);
+printf("Enter the endinglimit \n");
+scanf("%d",endinglimit);
+for(k=startinglimit+1;k<endinglimit;k++)
+{
+prime=1;
+if(k=2;k<n/2;k++)
+{
+if(n%2=0)
+prime=0;
+break;
+}
+if(prime)
+printf("%d\n",k);
+}
+}
