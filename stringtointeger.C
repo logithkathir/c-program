c22820c8b992b4e44dfc695a83216d791803d638
sasikala <kanniahjammuna@gmail.com>
2017-06-28 09:52:04 +0530

    stringtointeger.C
    program for string to integer conversion

---
+++ stringtointeger.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+int num;
+char marks[3];
+printf("Enter the marks\n");
+scanf("%d",&marks);
+num=atoi(marks);
+printf("\n marks :%d",num);
+return 0;
+}
