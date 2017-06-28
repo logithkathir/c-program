d9032054759274d2a4b494020f8ada7fe14765ed
sasikala <kanniahjammuna@gmail.com>
2017-06-28 13:59:16 +0530

    Factorial.C
    write a program to find the Factorial of the given Number

---
+++ Factorialofgivennumber.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,num,k=1;
+printf("Enter the number \n");
+scanf("%d",&num);
+for(i=num;i<=num;i++)
+k=k*i;
+printf("The Factorial of the given number is : %d",num,k);
+getch();
+}
