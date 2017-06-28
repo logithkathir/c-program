c6da56b3992d8e9a3613c88bf77dff17ab55b75c
sasikala <kanniahjammuna@gmail.com>
2017-06-28 11:18:39 +0530

    isomorphic.C
    program to find the given string is isomorphic

---
+++ isomorphic.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+char str1[10],str2[10];
+{
+printf("Enter the input strings \n");
+scanf("%s%s",&str1,&str2);
+if(m!=n)
+{
+printf("The given string is not an isomorphic string \n");
+else
+printf("The given string is an isomorphic string \n");
+return 0;
+}
+char compare_strings(string str1[],string str2[])
+int c=0;
+while(str1[c]==str2[c])
+{
+if(str1[c]=='\0'||str2[c]=='\0')
+}
+return 0;
+}
