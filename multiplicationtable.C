9ed89ef8e52cbfcb137a5f2a05d5bd9468fc64f5
sasikala <kanniahjammuna@gmail.com>
2017-06-28 14:08:41 +0530

    multiplicationtable.C
    Write a program to generate multiplication table

---
+++ multiplicationtable.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+int i,n;
+printf("Enter the input number \n");
+scanf("%d",&n)
+for(i=1;i<=n;++i)
+printf("%d*%d=%d\n",n,i,n*i);
+retun 0;
+}
