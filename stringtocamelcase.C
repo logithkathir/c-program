eaaf77ba64d2fe2eebc5f39c7b3fde103d1205b9
sasikala <kanniahjammuna@gmail.com>
2017-06-28 11:58:09 +0530

    stingtocamelcase.C
    write program to convert string to  camel case

---
+++ stringtocamelcase.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#inlcude<string.h>
+void main()
+{
+char arr[25];
+int i,k,l;
+printf("Enter the string \n");
+get(arr1);
+for(l=0;l<arr[l]!='\0';l++)
+if(arr1[]>='a'&&arr1[]<='z')
+arr1[0]=arr1[0]-32;
+}
+for(i=1;i<l;i++)
+{
+if(arr1[i]=='')
+{
+if(arr1[i+1]>='a'&&arr1[]<='z')
+int j=i+1;
+arr1[j]=arr1[j]-32;
+}
+}
+}
+printf("%s",arr1);
+return 0;
+}
