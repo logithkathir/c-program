5b85ead3f44edaeeb43d067a6772adb78c67b571
sasikala <kanniahjammuna@gmail.com>
2017-06-30 09:23:03 +0530

    printAscll.C
    program to print the Ascll values

---
+++ printAscll.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int ivo=0;
+clrscr();
+while(ivo<=255)
+{
+printf("%d%c\n",ivo,ivo);
+ivo++;
+}
+prrintf("Press any key to proceed");
+return 0;
+}
