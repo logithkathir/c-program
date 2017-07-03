2196342a6f40e8e4b6e419f585a7d064c54b5547
sasikala <kanniahjammuna@gmail.com>
2017-07-03 14:43:53 +0530

    swap.C
    program to swap two numbers

---
+++ swap.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int temp,firstnumber,secondnumber;
+printf("Enter the first number \n");
+scanf("%d",&firstnumber);
+printf("Enter the second number \n");
+scanf("%d",secondnumber);
+temp=firstnumber;
+firstnumber=secondnumber;
+second number=temp;
+printf("After swapping,first number=%lf\n",firstnumber);
+printf("After swapping,Second number=%lf\n",secondnumber);
+return 0;
+}
